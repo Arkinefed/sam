81ca3d041dbff25557d6fa19e319b4f3b727ff68
Arkinefed <arkinefedmail@gmail.com>
2023-03-13 14:53:13 +0100

    dodano wyświetlanie odtwarzaczy

--- lab1/source/index.php
+++ lab1/source/index.php
@@ -1,1 +1,35 @@
-<?php echo "Hello World!\n"; ?>
+<?php
+$renderVideo = isset($_GET["videoFile"]);
+$renderAudio = isset($_GET["audioFile"]);
+
+
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <title>Hello World Player</title>
+</head>
+
+<body>
+
+    <?php if ($renderAudio) {  ?>
+
+        <audio id="audioPlayer" src="<?php echo ($_GET["audioFile"]); ?>">
+
+        </audio>
+
+    <?php } else if ($renderVideo) { ?>
+
+        <video id="videoPlayer" src="<?php echo ($_GET["videoFile"]); ?>">
+
+        </video>
+
+    <?php } else { ?>
+
+    <?php } ?>
+
+</body>
+
+</html>
